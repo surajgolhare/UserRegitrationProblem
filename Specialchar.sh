ba98e5a3e4cee8be546952e3858711acf02355fd
unknown <you@example.com>
2020-06-08 23:53:04 +0530

    Password Contain At least one special character

---
+++ Specialchar.sh
@@ -0,0 +1,13 @@
+shopt -s extglob
+
+read -p "Enter the Password number: " Password
+function validate()
+{
+        if [[ ${#1} -ge 8 && "$1" == *[A-Z]* && "$1" == *[a-zA-Z]* && "$1" == *[0-9]* && "$1" == *[@#$%]* ]]
+        then
+                echo "this is Correct Password"
+        else
+                echo "this is Wrong Password"
+        fi
+}
+validate $Password
